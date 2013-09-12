4e2be6562bf5fb885120aabd3a381fc4d6986944
Joseph Plant <git@JosephPlant.com>
2013-09-13 08:29:08 +1000

    ReadOnlyFieldMapping tests

---
+++ src/AmplaWeb.Data.Tests/Binding/Mapping/ReadOnlyFieldMappingUnitTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AmplaWeb.Data.Attributes;
+using AmplaWeb.Data.Binding.ModelData;
+using AmplaWeb.Data.Tests;
+using NUnit.Framework;
+
+namespace AmplaWeb.Data.Binding.Mapping
+{
+    [TestFixture]
+    public class ReadOnlyFieldMappingUnitTests : TestFixture
+    {
+        [AmplaLocation("Enterprise.Site.Point")]
+        [AmplaModule("Production")]
+        public class Model
+        {
+            public Model() : this("EquipmentId"){ }
+            public Model(string equipmentId)
+            {
+                EquipmentId = equipmentId;
+            }
+
+            public int Id { get; set; }
+
+            public string EquipmentId { get; private set; }
+        }
+
+        [Test]
+        public void ResolveValue()
+        {
+            ReadOnlyFieldMapping fieldMapping = new ReadOnlyFieldMapping("EquipmentId");
+
+            Model model = new Model("EQ123") { Id = 100 };
+
+            ModelProperties<Model> modelProperties = new ModelProperties<Model>();
+
+            string value;
+            Assert.That(fieldMapping.TryResolveValue(modelProperties, model, out value), Is.False);
+            Assert.That(value, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void ReadWrite()
+        {
+            ReadOnlyFieldMapping fieldMapping = new ReadOnlyFieldMapping("EquipmentId");
+            Assert.That(fieldMapping.CanWrite, Is.False);
+            Assert.That(fieldMapping.CanRead, Is.True);
+        }
+
+    }
+}
