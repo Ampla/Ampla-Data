013b7963765fd79993ade1c62e7fa7cbc21f913b
Joseph Plant <git@JosephPlant.com>
2013-09-25 13:49:15 +1000

    Updated comments

--- src/AmplaWeb.Data/Binding/ViewData/IAmplaViewProperties.cs
+++ src/AmplaWeb.Data/Binding/ViewData/IAmplaViewProperties.cs
@@ -5,7 +5,15 @@ namespace AmplaWeb.Data.Binding.ViewData
 {
     public interface IAmplaViewProperties<in TModel>
     {
+        /// <summary>
+        ///     Gets the Permissions for the view
+        /// </summary>
         ViewPermissions Permissions { get; }
+        
+        /// <summary>
+        ///     Gets the Field Mappings for the view
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<FieldMapping> GetFieldMappings();
     }
 }
