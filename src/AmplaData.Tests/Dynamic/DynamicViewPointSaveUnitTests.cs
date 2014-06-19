dbd1386a38e68b2603949f98638c38e02fbd49ff
Joseph Plant <git@JosephPlant.com>
2014-06-19 20:04:38 +1000

    Added Dynamic Save Test

--- src/AmplaData.Tests/Dynamic/DynamicViewPointSaveUnitTests.cs
+++ src/AmplaData.Tests/Dynamic/DynamicViewPointSaveUnitTests.cs
@@ -41,5 +41,65 @@ namespace AmplaData.Dynamic
 
             Assert.That(amplaRecord.GetFieldValue("Sample Period", DateTime.MinValue), Is.InRange(before, after));
         }
+
+        public class ProductionModel
+        {
+            public string Location { get; set; }
+        }
+
+        [Test]
+        public void SaveUsingModelClass()
+        {
+            ProductionModel model = new ProductionModel {Location = location};
+
+            DateTime before = DateTime.UtcNow.AddSeconds(-1);
+
+            dynamic record = ViewPoint.Save(model);
+
+            DateTime after = before.AddSeconds(5);
+
+            Assert.That((object)record, Is.Not.Null);
+
+            Assert.That(record.Location, Is.EqualTo(location));
+
+            Assert.That(record.Id, Is.GreaterThan(0));
+
+            Assert.That(Records, Is.Not.Empty);
+
+            InMemoryRecord amplaRecord = Records[0];
+
+            Assert.That(amplaRecord, Is.Not.Null);
+
+            Assert.That(amplaRecord.Location, Is.EqualTo(location));
+            Assert.That(amplaRecord.Module, Is.EqualTo(module));
+
+            Assert.That(amplaRecord.GetFieldValue("Sample Period", DateTime.MinValue), Is.InRange(before, after));
+        }
+
+        //[Test]
+        //public void SaveWithSamplePeriod()
+        //{
+        //    DateTime local = DateTime.Today;
+        //    DateTime utc = local.ToUniversalTime();
+
+        //    var model = new { Location = location, SamplePeriod = local };
+        //    dynamic record = ViewPoint.Save(model);
+
+        //    Assert.That((object)record, Is.Not.Null);
+
+        //    Assert.That(record.Location, Is.EqualTo(location));
+        //    Assert.That(record.Id, Is.GreaterThan(0));
+
+        //    Assert.That(Records, Is.Not.Empty);
+        //    InMemoryRecord amplaRecord = Records[0];
+
+        //    Assert.That(amplaRecord, Is.Not.Null);
+
+        //    Assert.That(amplaRecord.Location, Is.EqualTo(location));
+        //    Assert.That(amplaRecord.Module, Is.EqualTo(module));
+
+        //    Assert.That(amplaRecord.GetFieldValue("Sample Period", DateTime.MinValue), Is.EqualTo(utc));
+            
+        //}
     }
 }
