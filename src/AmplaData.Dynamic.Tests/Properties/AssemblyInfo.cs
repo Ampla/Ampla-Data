1387296844d924726ba67d1fbc452f102497eda2
Joseph Plant <git@JosephPlant.com>
2014-12-05 09:19:09 +1000

    Restructured the Unit Tests

---
+++ src/AmplaData.Dynamic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AmplaData.Dynamic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("AmplaData.Dynamic.Tests")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4734230D-8CB5-4A46-A1C8-2C3F22E81717")]
+
+
